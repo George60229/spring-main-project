cea5860995c13b0cb2a1bae8d080c15b92a63d02
George Pashnev <100297574+George60229@users.noreply.github.com>
2023-02-27 17:51:03 +0200

    Update SecurityApplication.java

--- src/main/java/com/esm/security/SecurityApplication.java
+++ src/main/java/com/esm/security/SecurityApplication.java
@@ -1,9 +1,14 @@
 package com.esm.security;
 
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootTomcatApplication extends SpringBootServletInitializer {
+public class SecurityApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
 }
